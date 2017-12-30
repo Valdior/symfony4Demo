9516cba54ef9031c357452ae8242beddcb844cb0
Valdior <marechal.pierre@live.be>
2017-12-30 23:23:43 +0100

    init Post

---
+++ src/Entity/Post.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $published;
+
+    public function __construct()
+    {
+
+    }    
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of title
+     */ 
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @return  self
+     */ 
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of published
+     */ 
+    public function getPublished()
+    {
+        return $this->published;
+    }
+
+    /**
+     * Set the value of published
+     *
+     * @return  self
+     */ 
+    public function setPublished($published)
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+}
